e150c9499b7b2491a7dd549af5ccb3b5ed19d891
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-21 14:18:01 +0200

    Created bin/template.py

---
+++ bin/netinfo.py
@@ -0,0 +1,19 @@
+"""netinfo
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-9-21"
+
+class netinfo:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(netinfo.netinfo())
+	
+	def netinfo():
+		"""netinfo first funtion
+		"""
+		return "netinfo" #for the test (see test_netinfo.py)
+		
+if __name__ == "__main__":
+	netinfo()
