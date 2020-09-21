3537bce2a26e93b0959d9b5c5dab85b549f50f48
DavideC03 <davide.castellani.5.8.03@gmail.com>
2020-09-21 16:11:34 +0200

    v01.01

--- bin/netinfo.py
+++ bin/netinfo.py
@@ -1,19 +1,92 @@
 """netinfo
 """
+import os, wmi, sqlite3
+from datetime import datetime
 
 __author__ = "help@castellanidavide.it"
-__version__ = "1.0 2020-9-21"
+__version__ = "01.01 2020-09-21"
 
 class netinfo:
-	def __init__ (self):
-		"""Where it all begins
+	def __init__ (self, debug=False):
+		"""The core of my project
 		"""
-		print(netinfo.netinfo())
-	
-	def netinfo():
-		"""netinfo first funtion
-		"""
-		return "netinfo" #for the test (see test_netinfo.py)
+		base_dir = "." if debug else ".." # the project "root" in Visual studio it is different
+
+		log = open(os.path.join(base_dir, "log", "trace.log"), "a")
+		csv_names = open(os.path.join(base_dir, "flussi", "computers.csv"), "r")
+		csv_netinfo = open(os.path.join(base_dir, "flussi", "netinfo.csv"), "w+")
+		db_netinfo = sqlite3.connect(os.path.join(base_dir, "flussi", "netinfo.db")).cursor()
+
+		netinfo.log(log, "Opened all files and database connected")
+
+		intestation = "Caption,Description,Status,Manufacturer,Name,GuaranteesDelivery,GuaranteesSequencing,MaximumAddressSize,MaximumMessageSize,SupportsConnectData,SupportsEncryption,SupportsGracefulClosing,SupportsGuaranteedBandwidth,SupportsQualityofService"
+		netinfo.init_csv(csv_netinfo,intestation, log)
 		
+		start_time = datetime.now()
+		netinfo.log(log, f"Start time: {start_time}")
+		netinfo.log(log, "Running: netinfo.py")
+
+		netinfo.print_all(log, csv_names, csv_netinfo, db_netinfo, debug)
+		netinfo.check_db(db_netinfo, intestation, log)
+
+		netinfo.log(log, f"End time: {datetime.now()}\nTotal time: {datetime.now() - start_time}")
+		netinfo.log(log, "")
+		log.close()
+
+	def print_all(log, csv_names, csv_netinfo, db_netinfo, debug=False):
+		"""Prints the infos by Win32_NetworkClient & Win32_NetworkProtocol
+		"""
+		for PC_name in ("My PC, debug option",) if debug else csv_names.read().split("\n")[1:]:
+			
+			conn = wmi.WMI("" if debug else PC_name)
+			netinfo.print_and_log(log, f" - {PC_name}")
+
+			for network_client, network_protocol in zip(conn.Win32_NetworkClient(["Caption", "Description", "Status", "Manufacturer", "Name"]), conn.Win32_NetworkProtocol(["GuaranteesDelivery", "GuaranteesSequencing", "MaximumAddressSize", "MaximumMessageSize", "SupportsConnectData", "SupportsEncryption", "SupportsEncryption", "SupportsGracefulClosing", "SupportsGuaranteedBandwidth", "SupportsQualityofService"])):
+				netinfo.print_and_log(log, "   - Istructions: Win32_NetworkClient && Win32_NetworkProtocol")
+				data = f"'{network_client.Caption}','{network_client.Description}','{network_client.Status}','{network_client.Manufacturer}','{network_client.Name}','{network_protocol.GuaranteesDelivery}','{network_protocol.GuaranteesSequencing}','{network_protocol.MaximumAddressSize}','{network_protocol.MaximumMessageSize}','{network_protocol.SupportsConnectData}','{network_protocol.SupportsEncryption}','{network_protocol.SupportsGracefulClosing}','{network_protocol.SupportsGuaranteedBandwidth}','{network_protocol.SupportsQualityofService}'"
+				csv_netinfo.write(data.replace("'", ""))
+				db_netinfo.execute(f"INSERT INTO netinfo VALUES ({data})")
+
+	def log(file, item):
+		"""Writes a line in the log.log file
+		"""
+		file.write(f"{item}\n")
+
+	def print_and_log(file, item):
+		"""Writes on the screen and in the log file
+		"""
+		print(item)
+		netinfo.log(file, item)
+
+	def init_db(db_netinfo, intestation, log):
+		"""Init the database
+		"""
+		try:
+			netinfo.init_db(db_netinfo, intestation, log)
+		except:
+			db_netinfo.execute("DROP TABLE netinfo")
+			netinfo.init_db(db_netinfo,intestation, log)
+
+		db_netinfo.execute(f'''CREATE TABLE netinfo ({intestation})''')
+		netinfo.log(log, "database now initialized")
+
+	def init_csv(csv_netinfo, intestation, log):
+		"""Init the csv files
+		"""
+		csv_netinfo.write(f"{intestation}\n")
+		netinfo.log(log, "csv now initialized")
+
+	def check_db(db_file, intestation, log, tablename="netinfo"):
+		"""Checks the database content
+		"""
+		print("\nDatabase content:")
+		print(f"{intestation}")
+
+		for row in db_file.execute(f"SELECT * FROM {tablename}"):
+			print(str(row)[1:-1].replace("'", ""))
+
 if __name__ == "__main__":
-	netinfo()
+	# degub flag
+	debug = True
+
+	netinfo(debug)
--- docs/README.md
+++ docs/README.md
@@ -1,25 +1,27 @@
 # netinfo
-[![GitHub license](https://img.shields.io/badge/licence-GNU-green?style=flat)](https://github.com/CastellaniDavide/cpp-netinfo/blob/master/LICENSE) ![Author](https://img.shields.io/badge/author-Castellani%20Davide-green?style=flat) ![Version](https://img.shields.io/badge/version-v1.0-blue?style=flat) ![Language Python](https://img.shields.io/badge/language-Python-yellowgreen?style=flat) ![sys.platform supported](https://img.shields.io/badge/OS%20platform%20supported-Linux,%20Windows%20&%20Mac%20OS-blue?style=flat) [![On GitHub](https://img.shields.io/badge/on%20GitHub-True-green?style=flat&logo=github)](https://github.com/CastellaniDavide/netinfo)
+[![GitHub license](https://img.shields.io/badge/licence-GNU-green?style=flat)](https://github.com/CastellaniDavide/cpp-netinfo/blob/master/LICENSE) ![Author](https://img.shields.io/badge/author-Castellani%20Davide-green?style=flat) ![Version](https://img.shields.io/badge/version-v01.01-blue?style=flat) ![Language Python](https://img.shields.io/badge/language-Python-yellowgreen?style=flat) ![sys.platform supported](https://img.shields.io/badge/OS%20platform%20supported-Windows-blue?style=flat) [![On GitHub](https://img.shields.io/badge/on%20GitHub-True-green?style=flat&logo=github)](https://github.com/CastellaniDavide/netinfo)
 
 # Description
-Get net infos in all computers in "computers.csv" file
+Get net infos in all computers in "computers.csv" file & in "computers.db" database
 
 ## Required
  - python3
+ - pip3 librerias (in project core: pip3 install -r requirements/requirements.txt)
  
 ## Directories structure
  - .github
    - ISSUE_TEMPLATE
      - bug_report.md
      - feature-request.md
-   - workflows
-     - python-test.yml
  - bin
    - netinfo.py
-   - test_netinfo.py
  - docs
    - LICENSE
    - README.md
+ - flussi
+   - computers.csv
+   - netinfo.csv
+   - netinfo.db
  - log
    - trace.log
  - requirements
@@ -27,12 +29,11 @@ Get net infos in all computers in "computers.csv" file
    
 ### Execution examples
  - python3 netinfo.py
- - python3 test_netinfo.py
 
 # Changelog
- - [Version_1.0_2020-9-21](#Version_10_2020-9-21)
+ - [Version_01.01_2020-9-21](#Version_0101_2020-9-21)
 
-## Version_1.0_2020-9-21
+## Version_01.01_2020-9-21
  - Initial version
 
 ---
